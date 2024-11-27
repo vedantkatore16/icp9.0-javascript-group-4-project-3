5683ab2f680b5772702f0be13cabf9c656770705
NikitaJagtap2003 <jagtapnikita541@gmail.com>
2024-11-27 23:36:51 +0530

    page create

--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,1 @@
 
-const quantElement = document.getElementById('quantity'); 
-
-function incre(){
-  let quantity = parseInt(quantElement.innerText);
-  quantElement.innerText = ++quantity;
-}
-function dece(){
-    let quantity = parseInt(quantElement.innerText);
-    if(quantity>1){
-        quantElement.innerText = --quantity;
-    }
-}
-
