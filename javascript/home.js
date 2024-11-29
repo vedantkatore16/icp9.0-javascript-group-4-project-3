8b67bb0bc5532c125bc11e32fb652dcbffd5914d
GitanjaliSase <sasegitanjali20@gmail.com>
2024-11-29 12:10:44 +0530

    js file add

---
+++ javascript/home.js
@@ -0,0 +1,8 @@
+const products = [
+    {
+        "category": "Books, Toys & More",
+        "items": [
+            { "name": "Helmet", "price": "From ₹699", "image": "./../images/home-page/electronics.jpg", "link": "https://example.com/helmet" },
+        ]
+    }
+]
