e3678ccf176c0a32ebf314ba862e4a6c550bf36a
NikitaJagtap2003 <jagtapnikita541@gmail.com>
2024-11-30 12:50:40 +0530

    rating added to all

--- javascript/script.js
+++ javascript/script.js
@@ -35,23 +35,14 @@ function MoveImage(num){
     function order(){
       alert('Your order is confirmed...');
          }
+
+  const stars = document.querySelectorAll(".stars i");
+   stars.forEach((star, index1) => {
+    star.addEventListener("click", () => {
     
-    function rating(num){
-      const rate = document.getElementById('rate');
-      if(num == 1){
-          rate.src = "../images/fashion-page/rate1.png";  
-      } 
-      else if(num == 2){
-        rate.src = "../images/fashion-page/rate2.png";
-      }
-      else if(num == 3){
-        rate.src = "../images/fashion-page/rate3.png";
-      }
-      else if(num == 4){
-        rate.src = "../images/fashion-page/rate4.png";
-      }
-      else if(num == 5){
-        rate.src = "../images/fashion-page/rate5.png";
-      }
-    } 
+      stars.forEach((star,index2) => {
+        index1 >= index2 ? star.classList.add("active") : star.classList.remove("active");
+      });
+    });
+   });
 
