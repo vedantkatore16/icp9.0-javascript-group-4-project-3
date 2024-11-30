c5304b2ed5d65b9ead4c76929d961b506168ba1d
GitanjaliSase <sasegitanjali20@gmail.com>
2024-11-30 15:38:22 +0530

    homecss add

--- javascript/home.js
+++ javascript/home.js
@@ -1,8 +1,33 @@
 const products = [
     {
-        "category": "Books, Toys & More",
+        "category": "",
         "items": [
-            { "name": "Helmet", "price": "From ₹699", "image": "./../images/home-page/electronics.jpg", "link": "https://example.com/helmet" },
+            { "name": "Helmet", "price": "From ₹699", "image": "./../images" },
+            { "name": "Trendy Kurtas", "price": "Under ₹499", "image": "../ima" }
+
         ]
     }
-]
+];
+displayProducts()
+function displayProducts() {
+    const productList = document.getElementById("product-list");
+
+    products.forEach(category => {
+        const categoryTitle = document.createElement("h2");
+        categoryTitle.textContent = category.category;
+        productList.appendChild(categoryTitle);
+
+        category.items.forEach(item => {
+            const product = document.createElement("div");
+            product.classList.add("product");
+
+            product.innerHTML = `
+                <img src="${item.image}" alt="${item.name}">
+                <h2>${item.name}</h2>
+                <p>${item.price}</p>
+            `;
+
+            productList.appendChild(product);
+        });
+    });
+}
