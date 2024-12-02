cca74cb85a14bb259c3a6569065a1e0d8b84050d
rushikesh-kusekar <kusekarrushikesh45@gmail.com>
2024-12-02 17:53:20 +0530

    heading section updated

--- javascript/toys.js
+++ javascript/toys.js
@@ -1,67 +1,27 @@
-function MoveImage(num){
-    const mainImage = document.getElementById('main');
-      if(num == 1){
-        mainImage.src = "../../images/fashion-page/saree1.png";
-      }
-      else if(num == 2){
-        mainImage.src = "../../images/fashion-page/saree2.png";
-      }
-      else if(num == 3){
-        mainImage.src = "../../images/fashion-page/sarree3.png";
-      }
-      else if(num == 4){
-        mainImage.src = "../../images/fashion-page/saree1.png";
-      }
-      else if(num == 5){
-        mainImage.src = "../../images/fashion-page/sarree5.png";
-      }
+const images = ["./../images/watch-img/banner-img2.webp", "./../images/watch-img/banner-img.webp", "./../images/watch-img/banner-img3.webp", "./../images/watch-img/banner-img4.webp"];
+let index = 0;
+const slide = document.getElementById('slider');
+
+
+function forward() {
+    if (index < images.length - 1) {
+        index++;
     }
-    
-    const quantElement = document.getElementById('quantity'); 
-    
-    function incre(){
-      let quantity = parseInt(quantElement.innerText);
-      quantElement.innerText = ++quantity;
-    }
-    function dece(){
-        let quantity = parseInt(quantElement.innerText);
-        if(quantity>1){
-            quantElement.innerText = --quantity;
-        }
+    else {
+        index = 0;
     }
 
-    function order(){
-      alert('Your order is confirmed...');
-         }
+    slide.src = images[index]
+}
 
-
-  const stars = document.querySelectorAll(".stars i");
-   stars.forEach((star, index1) => {
-    star.addEventListener("click", () => {
-    
-      stars.forEach((star,index2) => {
-        index1 >= index2 ? star.classList.add("active") : star.classList.remove("active");
-      });
-    });
-   });
-
-   function changeImage(fileName){
-    let img = document.querySelector("#bannerImage");
-    img.setAttribute("src",fileName)
-  }
-
-   function randomImage(fileName){
-    let img = document.querySelector("#shoesImage");
-    img.setAttribute("src",fileName)
-  }
-
-  function sadiImage(fileName){
-    let img = document.querySelector("#newSadi");
-    img.setAttribute("src",fileName)
-  }
-
-  function clickImage(fileName){
-    let img = document.querySelector("#rounded");
-    img.setAttribute("src",fileName)
-  }
+function backward() {
+    if (index > 0) {
+        index--;
+    }
+    else {
+        index = images.length - 1;
+    }
+    slide.src = images[index]
+}ute("src",fileName)
+  
    
